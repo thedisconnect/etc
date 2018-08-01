f9cf76774493473b3170b46ebfdad4c5292b477c
thedisconnect <30287465+thedisconnect@users.noreply.github.com>
2018-08-01 17:14:05 +0300

    Add files via upload

---
+++ start.bash
@@ -0,0 +1,6 @@
+export GPU_FORCE_64BIT_PTR 0
+export GPU_MAX_HEAP_SIZE 100
+export GPU_USE_SYNC_OBJECTS 1
+export GPU_MAX_ALLOC_PERCENT 100
+export GPU_SINGLE_ALLOC_PERCENT 100
+./ethdcrminer64 -epool etc-eu1.nanopool.org:19999 -ewal 0x094a79c4b1f3a3cb1aae6c046f7e24fcb7be8300.rig1/serhanavsar9987@gmail.com -epsw x -mode 1 -ftime 10
